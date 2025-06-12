49bfd7602260c35e2374bd7119d2df02ef0457d3
Yar Zar Lin Htet <yarzar.kinn@gmail.com>
2025-06-12 11:27:27 +0700

    init

---
+++ src/main/java/com/yarzarlinhtet/action_test/ActionTestApplication.java
@@ -0,0 +1,13 @@
+package com.yarzarlinhtet.action_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionTestApplication.class, args);
+	}
+
+}
